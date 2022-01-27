bdc7814753ad3db087c9c3b27010d7bef2b86966
NJaku01 <nick1212@gmx.de>
2022-01-27 18:37:23 +0100

    send movement and environment

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
-from flask import Flask, render_template, request, jsonify
-from flask_wtf import FlaskForm
-from wtforms import FileField
+from flask import Flask, render_template, request, jsonify, send_from_directory
+
 from flask_mongoengine import MongoEngine
 import json
-from flask_uploads import configure_uploads, IMAGES, UploadSet
+from flask_uploads import configure_uploads, IMAGES, UploadSet, AUDIO
 from classify_birds import classify
 import uuid
 
@@ -11,7 +10,8 @@ import uuid
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = 'thisisasecret'
-app.config['UPLOADED_IMAGES_DEST'] = 'static/data/images'
+app.config['UPLOADED_IMAGES_DEST'] = 'uploads/images'
+app.config['UPLOADED_AUDIOS_DEST'] = 'uploads/audios'
 app.config['MONGODB_SETTINGS'] = {
     'db': 'your_database',
     'host': 'localhost',
@@ -21,7 +21,11 @@ db = MongoEngine()
 db.init_app(app)
 
 images = UploadSet('images', IMAGES)
-configure_uploads(app, images)
+
+
+audios = UploadSet('audios', AUDIO)
+configure_uploads(app, (images, audios))
+
 
 
 
@@ -31,19 +35,19 @@ class Location(db.EmbeddedDocument):
 
 class Environment(db.DynamicEmbeddedDocument):
     env_id = db.StringField()
-    date = db.DateTimeField()
-    temp = db.StringField()
+    date = db.StringField()
+    
 
 class Detection(db.DynamicEmbeddedDocument):
     det_id = db.StringField()
-    date = db.DateTimeField()
+    date = db.StringField()
     image = db.URLField()
     weight = db.FloatField()
 
 class Movements(db.DynamicEmbeddedDocument):
     mov_id = db.StringField()
-    start_date =db.DateTimeField()
-    end_date = db.DateTimeField()
+    start_date =db.StringField()
+    end_date = db.StringField()
     audio= db.URLField()
     environment = db.EmbeddedDocumentField(Environment)
     detections = db.ListField(db.EmbeddedDocumentField(Detection))
@@ -78,11 +82,23 @@ def image():
         # this line prints out the form to the browser
         #return jsonify(data)
         filename = images.save(data)
-        result = classify('static/data/images/' + filename)
+        result = classify('uploads/images/' + filename)
 
         return jsonify(
             result=result
     )
+    return render_template('./index.html')
+
+@app.route('/audio', methods=['POST'])
+def audio():
+    if request.method=="POST":
+        # this line goes to the console/terminal in flask dev server
+        data = request.files['audio']
+        print (data)
+        # this line prints out the form to the browser
+        #return jsonify(data)
+        filename = audios.save(data)
+        return filename
     return render_template('./index.html')
 
 @app.route('/box', methods=['GET', 'POST'])
@@ -112,6 +128,36 @@ def get_one_box(box_id: str):
     box = Box.objects(box_id=box_id).first_or_404()
     return jsonify(box), 200
 
+@app.route('/environment/<box_id>', methods=['POST'])
+def add_environment(box_id: str):
+
+    box = Box.objects(box_id=box_id).first_or_404()
+
+    ##TODO: Count Birds in image and Crop images to bird onyl 
+    
+    
+
+    body = request.get_json()
+    body=body[0]
+    print (body)
+
+    environmentClass = Environment()
+    for name,value in body.items():
+        setattr(environmentClass, name, value)
+    env_id = str(uuid.uuid4())
+    environmentClass.env_id = env_id
+
+    
+    
+    environmentList = box.measurements.environment
+    environmentList.insert(0, environmentClass)
+    box.measurements.environment = environmentList
+    box.update(measurements = box.measurements)
+
+#TODO send E-Mail
+
+    return jsonify(box), 200
+
 @app.route('/movement/<box_id>', methods=['POST'])
 def add_movement(box_id: str):
     content_type = request.headers.get('Content-Type')
@@ -123,33 +169,45 @@ def add_movement(box_id: str):
     
 
     body = request.form['json']
+    print(body)
+    body = json.loads(body)
+    print(body.items())
 
 
 
     movementsClass = Movements()
     mov_id = str(uuid.uuid4())
     movementsClass.mov_id = mov_id
-    movementsClass.start_date = body.start_date
-    movementsClass.end_date = body.end_date
+    movementsClass.start_date = body['start_date']
+    movementsClass.end_date = body['end_date']
+    audio = request.files[body['audio']]
+    filename = audios.save(audio)
+    movementsClass.audio = "http://localhost:5000/uploads/audios/" + filename
+
+    environmentClass = Environment()
+    for name,value in body['environment'].items():
+        setattr(environmentClass, name, value)
+    env_id = str(uuid.uuid4())
+    environmentClass.env_id = env_id
+
+    movementsClass.environment = environmentClass
 
     
-
-    
-    for detection in body.detections:
-        image = request.files[detection.image]
+    for detection in body['detections']:
+        image = request.files[detection['image']]
         detectionClass = Detection()
 
     # this line prints out the form to the browser
     #return jsonify(data)
         filename = images.save(image)
-        result = classify('static/data/images/' + filename)
+        result = classify('uploads/images/' + filename)
         if result[0][0] > 0.5:
             bird_name = result[0][1]
             detectionClass.count = {bird_name: 1}
         det_id = str(uuid.uuid4())
         detectionClass.det_id = det_id
-        detectionClass.image = "http://localhost:5000/static/data/images/" + filename
-        detectionClass.weight = detection.weight
+        detectionClass.image = "http://localhost:5000/uploads/images/" + filename
+        detectionClass.weight = detection['weight']
         movementsClass.detections.append(detectionClass)
         
 
@@ -161,9 +219,21 @@ def add_movement(box_id: str):
     #filename = images.save(data)
     #result = classify('static/data/images/' + filename)
     movementList = box.measurements.movements
+    movementList.insert(0, movementsClass)
+    box.measurements.movements = movementList
+    box.update(measurements = box.measurements)
     print(movementList)
 
     return jsonify(box), 200
 
+@app.route('/uploads/images/<filename>')
+def uploadImages(filename):
+    return send_from_directory(app.config['UPLOADED_IMAGES_DEST'], filename)
+
+@app.route('/uploads/audios/<filename>')
+def uploadAudios(filename):
+    return send_from_directory(app.config['UPLOADED_AUDIOS_DEST'], filename)
+
+
 if __name__==('__main__'):
-    app.run(host="0.0.0.0", debug=True)
+    app.run(host="0.0.0.0", debug=False)
--- request.py
+++ request.py
@@ -1,21 +1,21 @@
 import json
 import requests
 import os
-def send_request(file):
+def send_request(file, file2):
     payload = {
-        "start_date" : 123,
-        "end_date" : 123,
+        "start_date" : "123",
+        "end_date" : "123",
         "audio" : "audioKey",
         "environment" : {
-            "date": 123,
+            "date": "123",
             "temp": 10
         },
         "detections" : [{
-            "date": 123,
+            "date": "123",
             "image": "imageKey",
             "weight" : 7.6},
             {
-            "date": 123,
+            "date": "123",
             "image": "imageKey2",
             "weight" : 7.6} ]
 
@@ -24,9 +24,30 @@ def send_request(file):
          'json': (None, json.dumps(payload), 'application/json'),
          'imageKey': (os.path.basename(file), open(file, 'rb')),
          'imageKey2': (os.path.basename(file), open(file, 'rb')),
-         'audioKey': (os.path.basename(file), open(file, 'rb'))
+         'audioKey': (os.path.basename(file2), open(file2, 'rb'), 'audio/mpeg')
     }
     r = requests.post("http://localhost:5000/movement/fc4ae0c9-bee0-44e8-9c2f-d86c5e932294", files=files)
     print(r.content)
 
-send_request("./static/data/images/svetozar-cenisev-pvqTCIOx9MQ-unsplash.jpg")
+#send_request("./static/data/images/svetozar-cenisev-pvqTCIOx9MQ-unsplash.jpg", "test.mp3")
+
+def send_environment(file, file2):
+    payload = {
+            "date": "123",
+            "temp": 10
+        },
+       
+
+    r = requests.post("http://localhost:5000/environment/fc4ae0c9-bee0-44e8-9c2f-d86c5e932294", json=payload)
+    print(r.content)
+
+#send_environment("./static/data/images/svetozar-cenisev-pvqTCIOx9MQ-unsplash.jpg", "test.mp3")
+
+def send_audio(file, file2):
+  
+    files = {
+         'audio': (os.path.basename(file2), open(file2, 'rb'), 'audio/mpeg')
+    }
+    r = requests.post("http://localhost:5000/audio", files=files)
+
+send_audio("./static/data/images/svetozar-cenisev-pvqTCIOx9MQ-unsplash.jpg", "test.mp3")
