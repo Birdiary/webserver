4e671c15e31f8efd8d6e0a75f380042189ead7df
n_jaku01@uni-muenster.de <n_jaku01@uni-muenster.de>
2023-10-29 16:22:00 +0100

    Modified 2 files

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjExMTY3MA/spectral.js?branch_slug=n_jaku01-modified-doc&token=ef436744-d313-4c10-8034-3a57368267e6";
+export default { extends: ruleset };
